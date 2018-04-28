5c752e326835ce5757e78cdd0fefc1df0d4c7b16
Vitaliy <vitaliy.drigola@gmail.com>
2018-04-28 22:09:46 +0300

    apgr proj

--- src/app/search/find/find.component.ts
+++ src/app/search/find/find.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {HttpBookService} from "../http-book.service";
 import { LocalStorageBooksService } from "../local-storage-books.service";
 
+export interface IBook {
+  isActiveButton: boolean;
+  id: string;
+}
+
 @Component({
   selector: 'app-find',
   templateUrl: './find.component.html',
@@ -10,15 +15,15 @@ import { LocalStorageBooksService } from "../local-storage-books.service";
 export class FindComponent implements OnInit {
 
   listBooks = [];
-  listLocalBooks =[];
+  listLocalBooks = [];
 
   constructor(
     private httpBooksService: HttpBookService,
     private localStorageBookService: LocalStorageBooksService
-
   ) { }
 
   ngOnInit() {
+    this.showBooks();
   }
 
   findBooks(value) {
@@ -36,22 +41,27 @@ export class FindComponent implements OnInit {
     }
 
     this.listLocalBooks = localStorageItems;
-    //return localStorageItems;
-
   }
 
 
   addBook(book) {
     if (this.localStorageBookService.getItem(book.id) === null) {
       this.localStorageBookService.setItem(book.id, book);
+      book.isActiveButton = true;
+      this.showBooks();
     } else {
       return;
     }
   }
 
-  deleteBook(id: string): void {
-    const resultDelete = this.localStorageBookService.deleteItem(id);
+  deleteBook(book: IBook): void {
+    const resultDelete = this.localStorageBookService.deleteItem(book.id);
 
+    const bookInSearchList = this.listBooks.find((el) => {
+      return el.id === book.id;
+    });
+
+    bookInSearchList.isActiveButton = false;
     if (resultDelete) {
       this.showBooks();
     }
--- src/app/search/http-book.service.ts
+++ src/app/search/http-book.service.ts
@@ -1,17 +1,30 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
+import {LocalStorageBooksService} from "./local-storage-books.service";
+import 'rxjs/Rx';
+
+interface IResponseBooksData {
+  items: Array<any>;
+}
 
 @Injectable()
   export class HttpBookService {
     constructor(
-      private http: HttpClient
+      private http: HttpClient,
+      private localStorageBooksService: LocalStorageBooksService
     ) {}
 
     getBooks(value: string): Observable<any> {
       const url = `https://www.googleapis.com/books/v1/volumes?q=${value}`;
 
-      return this.http.get(url);
+      return this.http.get(url)
+        .map((data: IResponseBooksData) => {
+          for (const unit of data.items) {
+            unit['isActiveButton'] = !!this.localStorageBooksService.getItem(unit.id);
+          }
+        return data;
+        });
     }
 
 }
